8e8d132f74420310448951a31e74fc285295f660
Himanshu Chaudhary <himanshu266@gmail.com>
2014-03-01 00:25:30 -0500

    finished face of the clock, added time in middle, internal workings now complete

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var timezones = {
     },
     'Paris' : {
         'offset' : +60,
-        'rotate' : 15
+        'rotate' : 15,
+        'height' : 45
     },
     'Rome' : {
         'offset' : +60,
@@ -18,12 +19,12 @@ var timezones = {
     'Cairo' : {
         'offset' : +120,
         'rotate' : 29,
-        'height' : 30
+        'height' : 45
     },
     'Baghdad' : {
         'offset' : +180,
         'rotate' : 45,
-        'height' : 68
+        'height' : 70
     },
     'Tehran' : {
         'offset' : +210,
@@ -36,12 +37,17 @@ var timezones = {
     },
     'Kabul' : {
         'offset' : +270,
-        'rotate' : 67.5,
+        'rotate' : 67,
         'height' : 45
     },
     'Delhi' : {
         'offset' : +330,
         'rotate' : 82.5
+    },
+    'Colombo' : {
+        'offset' : +330,
+        'rotate' : 83.5,
+        'height' : 45
     },
     'Bangkok' : {
         'offset' : +420,
@@ -49,27 +55,21 @@ var timezones = {
     },
     'Singapore' : {
         'offset' : +480,
-        'rotate' : 120,
-        'height' : 65
+        'rotate' : 119,
+        'height' : 45
     },
     'Beijing' : {
         'offset' : +480,
-        'rotate' : 120,
-        'height' : 45
-    },
-    'Manila' : {
-        'offset' : +480,
-        'rotate' : 120,
-        'height' : 90
+        'rotate' : 121
     },
     'Perth' : {
         'offset' : +525,
         'rotate' : 131.25,
-        'height' : 120
+        'height' : 70
     },
     'Seoul' : {
         'offset' : +540,
-        'rotate' : 135,
+        'rotate' : 136,
         'height' : 45
     },
     'Tokyo' : {
@@ -78,12 +78,12 @@ var timezones = {
     },
     'Sydney' : {
         'offset' : +600,
-        'rotate' : 150
+        'rotate' : 150,
+        'height' : 45
     },
     'Melbourne' : {
         'offset' : +600,
-        'rotate' : 150,
-        'height' : 45
+        'rotate' : 152
     },
     'Auckland' : {
         'offset' : +720,
@@ -95,7 +95,8 @@ var timezones = {
     },
     'Anchorage' : {
         'offset' : -540,
-        'rotate' : 225
+        'rotate' : 225,
+        'height' : 45
     },
     'Vancouver' : {
         'offset' : -480,
@@ -103,19 +104,21 @@ var timezones = {
     },
     'Los Angeles' : {
         'offset' : -480,
-        'rotate' : 240
+        'rotate' : 240,
+        'height' : 70
     },
     'Denver' : {
         'offset' : -420,
-        'rotate' : 255
+        'rotate' : 255,
+        'height' : 45
     },
     'Chicago' : {
         'offset' : -360,
         'rotate' : 270
     },
-    'Mexico City' : {
+    'Cancun' : {
         'offset' : -360,
-        'rotate' : 270,
+        'rotate' : 269,
         'height' : 70
     },
     'Belize' : {
@@ -129,12 +132,12 @@ var timezones = {
     },
     'New York' : {
         'offset' : -300,
-        'rotate' : 285,
+        'rotate' : 287,
         'height' : 45
     },
     'Lima' : {
         'offset' : -300,
-        'rotate' : 285,
+        'rotate' : 286,
         'height' : 70
     },
     'Rio' : {
@@ -168,8 +171,7 @@ function formatTime(date) {
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+    return hours + ':' + minutes + ' ' + ampm;
 }
 
 // Get time zone of city
@@ -181,21 +183,34 @@ function getTimezoneTime(city) {
 
 // Output all timezones
 var cities = document.getElementById('cities');
-for(var city in timezones) {
-    var el = document.createElement('div');
-    el.innerHTML = city;
-    el.setAttribute('data-city', city);
-    el.className = 'city';
-    if (lh = timezones[city].height) el.style.lineHeight = lh+'px';
-    el.style.webkitTransform = 'rotate('+(timezones[city].rotate+180)+'deg)';
-    cities.appendChild(el);
-}
+$(function() {
+    for(var city in timezones) {
+        var el = document.createElement('div');
+        var at = 70;
+        var zi = 3;
+        el.innerHTML = '<p>'+city+'</p>';
+        el.setAttribute('data-city', city);
+        el.className = 'city';
+        if (lh = timezones[city].height) {
+            el.style.lineHeight = lh+'px';
+            at = (lh == 45) ? 65 : 60;
+            zi = (lh == 45) ? 2 : 1;
+        }
+        el.style.zIndex = zi;
+        el.style.webkitTransform = 'rotate('+(timezones[city].rotate+180)+'deg)';
+        cities.appendChild(el);
+        $(el).find('p').arctext({radius: at});
+    }
 
-$('.city').arctext({radius: 90});
+    $('#data').html("Local Time<br>" + formatTime(new Date));
 
-$('.city').mouseover(function() {
-    var city = $(this).attr('data-city');
-    $('#data').html(city + ": " + formatTime(getTimezoneTime(city)));
+    // Show data on mouseover
+    $('.city p').mouseover(function() {
+        var city = $(this).parent().attr('data-city');
+        $('#data').html(city + "<br>" + formatTime(getTimezoneTime(city)));
+    }).mouseleave(function() {
+        $('#data').html("Local Time<br>" + formatTime(new Date));
+    });
 });
 
 // Rotate the face of the clock every second
